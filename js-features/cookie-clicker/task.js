04d770977220d5ca8f1775b7df6f6f0aa7343afe
Aleksei Barkasov <alekseibarkasov@gmail.com>
2025-04-12 19:16:21 +0300

    cookie-clicker v.1

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,22 @@
-fjhvjdh
+const clickerCounter = document.getElementById("clicker__counter");
+const cookie = document.getElementById("cookie");
+
+let cookieSize = cookie.width;
+let count = clickerCounter.textContent;
+
+let size = cookie.addEventListener("click", changeSize);
+let counter = cookie.addEventListener("click", addCounter);
+
+function changeSize() {
+  if (cookieSize === cookie.width) {
+    cookieSize *= 1.1;
+  } else {
+    cookieSize = cookieSize / 1.1;
+  }
+  cookie.width = cookieSize;
+}
+
+function addCounter() {
+  count++;
+  clickerCounter.textContent = count;
+}

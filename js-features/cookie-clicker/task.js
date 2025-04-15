e80e5ce5690134d1dbeab11f1c419fadd2eeb738
Aleksei Barkasov <alekseibarkasov@gmail.com>
2025-04-15 19:46:33 +0300

    clicker v2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,11 +1,15 @@
 const clickerCounter = document.getElementById("clicker__counter");
 const cookie = document.getElementById("cookie");
+const clickerCounterSpeed = document.getElementById("clicker__counter-speed");
 
 let cookieSize = cookie.width;
 let count = clickerCounter.textContent;
+let startTime = null;
+let click = 0;
 
 let size = cookie.addEventListener("click", changeSize);
 let counter = cookie.addEventListener("click", addCounter);
+let clickerSpeed = cookie.addEventListener("click", clickPerSecond);
 
 function changeSize() {
   if (cookieSize === cookie.width) {
@@ -19,4 +23,19 @@ function changeSize() {
 function addCounter() {
   count++;
   clickerCounter.textContent = count;
+}
+
+function clickPerSecond() {
+  if (!startTime) {
+    startTime = Date.now();
+  }
+  click++;
+
+  const elapsedSeconds = (Date.now() - startTime) / 1000;
+  const averageCPS = click / elapsedSeconds;
+
+  clickerCounterSpeed.textContent = averageCPS.toFixed(2);
+  
+  click = 0;
+  startTime = Date.now();
 }

0634a6c8f47bc3d96b5132ab4e819feeb5f61e5b
Aleksei Barkasov <alekseibarkasov@gmail.com>
2025-05-28 19:29:55 +0300

    text-editor V2

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,9 +2,7 @@ const editor = document.getElementById("editor");
 const saveText = localStorage.getItem("text");
 const clear = document.getElementById("clear");
 
-if (saveText) {
-  editor.value = saveText;
-}
+editor.value = saveText;
 
 editor.addEventListener("input", () => {
   localStorage.setItem("text", editor.value);
